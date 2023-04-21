0d0b97fa0947dc90fde15dc257c07fe97573f62d
zenraikou <zenraikou@gmail.com>
2023-04-21 21:33:57 +0800

    add serilog

--- Game.API/Program.cs
+++ Game.API/Program.cs
@@ -3,16 +3,26 @@ using Game.API.Data.IRepository;
 using Game.API.Data.Repository;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    var logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.FromLogContext().CreateLogger();
+
+    builder.Services.AddControllers().AddNewtonsoftJson();
+    builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+    builder.Logging.ClearProviders();
+    builder.Logging.AddSerilog();
+    
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
     builder.Services.AddDbContext<GameContext>(options => 
     {
         options.UseSqlServer(builder.Configuration.GetConnectionString("GameConnection"));
     });
-    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-    builder.Services.AddRouting(options => options.LowercaseUrls = true);
-    builder.Services.AddControllers().AddNewtonsoftJson();
+    
     builder.Services.AddMediatR(typeof(Program));
 }
 
